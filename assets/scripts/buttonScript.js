211b647094bb2f03dc14a8850bd7e5bee0509752
Rikucode <pavlyu4enko.a@gmail.com>
2022-04-11 02:11:36 +1000

    norm tak obnova

---
+++ assets/scripts/buttonScript.js
@@ -0,0 +1,3 @@
+document.querySelector("#FEFU").addEventListener("click", ()=>{
+  document.querySelector("#secret").style.visibility = "visible";
+});
---
+++ assets/scripts/textAnim.js
@@ -0,0 +1,10 @@
+// let progress = 0;
+
+window.addEventListener("load", ()=>{
+  setTimeout(()=>{
+    document.getElementById("text1").remove();
+  },3500);
+  setTimeout(()=>{
+    document.getElementById("text2").remove();
+  },6300);
+});
