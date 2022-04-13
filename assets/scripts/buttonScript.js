8d1cf849340e26398de3a3536e714c2a6bc7e229
Rikucode <pavlyu4enko.a@gmail.com>
2022-04-14 04:01:07 +1000

    final
    really

--- assets/scripts/buttonScript.js
+++ assets/scripts/buttonScript.js
@@ -1,3 +1,6 @@
 document.querySelector("#FEFU").addEventListener("click", ()=>{
-  document.querySelector("#secret").style.visibility = "visible";
+  document.querySelector("#firstLine").innerHTML = "Да, ДВФУ - это лучший ВУЗ";
+  setTimeout(()=>{
+    document.querySelector("#firstLine").textContent = "Студент ДВФУ и просто крутой чел.";
+  },2000);
 });
